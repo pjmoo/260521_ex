47cc48513e4af641c779a8cae4a8ae7905a1bf9d
pjmoo <jung17min66@gmail.com>
2026-05-21 12:48:29 +0900

    Add array/object demo (05_objects-arr.js) and operator examples (06_operator.js)

---
+++ 06_operator.js
@@ -0,0 +1,42 @@
+console.log(30 + 7);
+console.log(30 - 7);
+console.log(30 * 7);
+console.log(30 / 7);
+console.log(30 % 7);
+console.log(30 - 5 * 3);
+console.log((30 - 5) * 3);
+let a = 0;
+a = a + 5;
+console.log(a);
+a += 10;
+console.log(a);
+console.log("100 ==10", 100 == 10);
+console.log("100 >=10", 100 >= 10);
+console.log("10 >=10", 10 >= 10);
+console.log("100 <=10", 10 <= 10);
+console.log("10 <= 10", 10 <= 10);
+console.log("100 != 10", 10 != 10);
+console.log('"고양이" == "개"', "고양아" == "개");
+console.log('"고양이" != "개"', "고양아" != "개");
+console.log('"개" == "개"', "개" == "개");
+
+console.log(1+"1");
+console.log(1 + "1" * 1);
+console.log(2 * "2");
+
+console.log(1 == "1");
+
+console.log(1 === "1");
+console.log(1 !== "1");
+console.log(0 == "0");
+console.log("" == 0);
+console.log("" == "0");
+
+console.log("-----------")
+const b1 = 100>10;
+console.log(b1);
+console.log(!b1);
+const b2 = 50<20;
+console.log(b2);
+console.log(b1 && b2);
+console.log(b1 || b2);
